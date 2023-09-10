13fa793eacbf300dfbb49771f8491c2e9a1da30f
developer-heyy <121794924+haeyoon706@users.noreply.github.com>
2023-09-10 23:08:00 +0900

    로그인폼 만들기

--- my-diary/src/pages/login/Login.js
+++ my-diary/src/pages/login/Login.js
@@ -1,5 +1,49 @@
 import styles from "./Login.module.css";
+import { useState } from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleData = (event) => {
+    if (event.target.type === "email") {
+      setEmail(event.target.value);
+    } else if (event.target.type === "password") {
+      setPassword(event.target.value);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form className={styles.login_form} onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>로그인</legend>
+        <label htmlFor="myEmail">email : </label>
+        <input
+          type="email"
+          id="myEmail"
+          required
+          value={email}
+          onChange={handleData}
+        />
+
+        <label htmlFor="myPassword">password : </label>
+        <input
+          type="password"
+          id="myPassword"
+          required
+          value={password}
+          onChange={handleData}
+        />
+
+        <button type="submit" className={styles.btn}>
+          로그인
+        </button>
+      </fieldset>
+    </form>
+  );
 }
