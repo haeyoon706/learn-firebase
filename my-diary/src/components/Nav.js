6bee1119aec893d4765427d96797394c0d886de2
developer-heyy <121794924+haeyoon706@users.noreply.github.com>
2023-09-19 21:14:50 +0900

    로그아웃 구현하기

--- my-diary/src/components/Nav.js
+++ my-diary/src/components/Nav.js
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import styles from "./Nav.module.css";
+import { useLogout } from "../hooks/useLogout";
 
 export default function Nav() {
+  const { logout } = useLogout();
+
   return (
     <nav className={styles.nav}>
       <h1 className={styles.tit}>두근 두근 비밀일기</h1>
@@ -11,6 +14,11 @@ export default function Nav() {
         </li>
         <li>
           <Link to="/signup">가입하기</Link>
+        </li>
+        <li>
+          <button type="button" onClick={logout}>
+            로그아웃
+          </button>
         </li>
       </ul>
     </nav>
